3efb480df8bc2f42d181f0ee280a3ecc32facdb4
zhornyk-maria <zhornyk.maria@gmail.com>
2024-02-02 16:07:14 +0200

    bug fix

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -24,7 +24,7 @@ form.addEventListener('submit', function(event) {
     promise.then(
         (delay) => {
             iziToast.success({
-                iconUrl: './img/success-icon.png',
+                icon: false,
                 messageColor: '#fff',
                 position: 'topRight',
                 titleColor: '#fff',
@@ -34,7 +34,7 @@ form.addEventListener('submit', function(event) {
         },
         (delay) => {
             iziToast.error({
-                iconUrl: './img/close-octagon.png',
+                icon: false,
                 messageColor: '#fff',
                 position: 'topRight',
                 animateInside: false,
